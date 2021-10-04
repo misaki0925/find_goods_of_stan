80f1b4e3441db1baf356a76861bcde30cead49cc
hoshinomisaki <guangsakux@gmail.com>
2021-10-04 21:25:12 +0900

    Add:seed_2

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
 @member = ["高地優吾", "京本大我", "田中樹", "松村北斗", "ジェシー", "森本慎太郎"]
 @member.each do |member|
-Member.create!{
+Member.create!(
   name: "#{member}"
-}
+)
 end
 
-User.create!{
+User.create!(
   email: ENV["EMAIL"]
   password: ENV["PASSWORD"]
-}
+)
