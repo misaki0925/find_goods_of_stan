9302478e1a7afb1346e76d2ea4c5a33374e0ee8b
hoshinomisaki <guangsakux@gmail.com>
2021-12-09 14:08:39 +0900

    Improve: Webhook

--- app/controllers/line_users_controller.rb
+++ app/controllers/line_users_controller.rb
@@ -13,27 +13,27 @@ class LineUsersController < ApplicationController
   def callback
     body = request.body.read
     signature = request.env['HTTP_X_LINE_SIGNATURE']
-    # unless client.validate_signature(body, signature)
-    #   error 400 do 'Bad Request' end
-    # end
-    events = client.parse_events_from(body)
-    events.each do |event|
-      text = event.message['text']
-      userId = event['source']['userId']
-      if text == "受け取るメンバーの設定をする"
-        message = {
-          "type": "text",
-          "text": "hello"
-        }
-      else
-        message = {
-          "type": "text",
-          "text": "こんにちは"
-        }
-      end
-      client.push_message(userId, message)
+    unless client.validate_signature(body, signature)
+      error 400 do 'Bad Request' end
     end
+    events = client.parse_events_from(body)
+
+    events.each do |event|
+      case event
+      when Line::Bot::Event::Message
+        case event.type
+        when Line::Bot::Event::MessageType::Text
+          message = {
+            type: 'text',
+            text: event.message['text']
+          }
+        end
+      end
+      client.reply_message(event['replyToken'], message)
+    end
+    head :ok
   end
+
 
   # LINEからメンバー設定のメッセージが届いたらメンバーを決めてもらう
     def setting_request
